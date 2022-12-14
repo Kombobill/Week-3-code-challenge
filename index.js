32420cb98f43a07273085dd43dde0ba23acf5112
Kombobill <118278282+Kombobill@users.noreply.github.com>
2022-12-14 11:28:39 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,76 @@
-const API = "http://localhost:3000/films/1";
-fetch(API)
-.then((res) => res.json())
-.then(renderFilm);
-function renderFilm(film) {
-    const filmMenuDiv = document.getElementById("film-menu");
-    const titleDiv = document.getElementById("film-details");
-    const runDiv = document.getElementById("runtime")
-    const showDiv = document.getElementById("showtime")
-    const availableDiv = document.getElementById("Available-tickets")
-    const filmPoster = document.createElement("img");
-    filmPoster.src = film.poster;
-    filmMenuDiv.append(filmPoster);
-    const filmTitle = document.createElement("p");
-    filmTitle.textContent = film.title;
-    titleDiv.append(filmTitle);
-    const filmRuntime = document.createElement("p");
-    filmRuntime.textContent = film.runtime;
-    runDiv.append(filmRuntime);
-    const filmShowtime = document.createElement("p");
-    filmShowtime.textContent = film.showtime;
-    showDiv.append(filmShowtime);
-    const filmAvailability = document.createElement("p");
-    filmAvailability.textContent = (film.capacity - film.tickets_sold);
-    availableDiv.append(filmAvailability);
-}
+document.addEventListener("DOMContentLoaded", () => {
+    // getting elements from the DOM
+    const moviesListContainer = document.querySelector("#moviesList");
+    const movieDetailsContainer = document.querySelector("#movieDetails");
+  
+    // getting films data
+    fetch("https://flatdango.vercel.app/db.json")
+      .then((res) => res.json())
+      .then((data) => {
+        displayMoviesList(data.films);
+      });
+  
+    //   displaying movies titles
+    function displayMoviesList(data) {
+      data.map((movie) => {
+        const markUp = `<li class="list-group-item" id="movieList">${movie.title}</li>`;
+  
+        // append movie titles
+        moviesListContainer.insertAdjacentHTML("afterbegin", markUp);
+  
+        // show the first movie details on the list on load of the page
+        displayMovieDetails(movie);
+  
+        // show movie details on click of the movie titles
+        const movieList = document.querySelector("#movieList");
+        movieList.addEventListener("click", () => {
+          displayMovieDetails(movie);
+        });
+      });
+    }
+  
+    // movie details
+    function displayMovieDetails(movie) {
+      const markUp = `<img src="${movie.poster}" alt="">
+    <div class="hello">
+    <h2 id="movieTitle" class= "customtitles">${movie.title}</h2>
+    <p id="description">${movie.description}</p>
+    <p id="runtime">Length: <span>${movie.runtime} Minutes</span></p>
+    <p id="showtime">Show Time: <span>${movie.showtime}</span></p>
+    <p id="capacity">Theater capacity: <span>${movie.capacity}</span></p>
+    <p id="ticketssold">Tickets sold: <span>${movie.tickets_sold}</span></p>
+    <p id="availableTickets">Available tickets: <span>${
+      movie.capacity - movie.tickets_sold
+    }</span></p>
+    <button class="custombtn" id="buyTicket">buy ticket </button>
+   </div>`;
+  
+      movieDetailsContainer.innerHTML = "";
+      movieDetailsContainer.insertAdjacentHTML("afterbegin", markUp);
+  
+      const btn = movieDetailsContainer.querySelector("button");
+      btn.addEventListener("click", () => {
+        buyTicket(movie);
+      });
+    }
+  
+    // ticket buying functionality
+    
+    function buyTicket(movie) {
+      const ticketsSold = document.querySelector("#ticketssold");
+      let remainingTickets = movie.capacity - movie.tickets_sold;
+      const btn = movieDetailsContainer.querySelector("button");
+      const availableTickets = document.querySelector("#availableTickets");
+  
+      if (remainingTickets > 0) {
+        movie.tickets_sold++;
+        remainingTickets--;
+        btn.innerHTML = "buy ticket";
+      } else {
+        btn.innerHTML = "sold out";
+        btn.classList.add("soldOut");
+      }
+      ticketsSold.innerHTML = `Tickets sold: <span>${movie.tickets_sold}</span>`;
+      availableTickets.innerHTML = `Available tickets: <span>${remainingTickets}</span>`;
+    }
+  });
